e989bd3c8af73e1b1815b7b8015f9047a33eb90e
huhx <huhx@sinoyd.com>
2018-08-28 11:11:28 +0800

    feater:分析列表编辑功能更新

--- src/views/third.vue
+++ src/views/third.vue
@@ -104,7 +104,7 @@
         <template slot-scope="scope">
             <el-button
             size="mini"
-            @click="handleEdit">编辑</el-button>
+            @click="isEditPop();editDialog(scope);">编辑</el-button>
             <el-button
             @click.native.prevent="handleDelete(scope.row)"
             size="mini"
@@ -114,8 +114,7 @@
     </el-table>
 
       <div class="analyseDialog">
-      <!-- dialog -->
-       <!-- 这里是analyse分析弹框内容 -->
+       <!-- add -- analyse弹框 -->
       <el-dialog
       title="add"
       :visible.sync="dialogVisible"
@@ -130,36 +129,30 @@
         class="demo-ruleForm">
 
         <!-- 因子 -->
-        <el-form-item label="因子" prop="factorName">
+        <el-form-item label="因子" prop="factor_title">
           <!-- 因子 -->
           <!-- 获取数据的下拉框 -->
-          <el-autocomplete
+          <el-input
           :disabled="disabled"
-          clearable
-          v-model="ruleForm.factorName"
-          :fetch-suggestions="querySearchAsync"
+          v-model="ruleForm.factor_title"
           placeholder="请输入内容"
-          @select="handleSelect"
-          ></el-autocomplete>
+          ></el-input>
         </el-form-item>
 
         <!-- 方法 -->
-        <el-form-item label="方法" prop="methodName">
+        <el-form-item label="方法" prop="method_title">
           <!-- 方法 -->
           <!-- 获取数据的下拉框 -->
-          <el-autocomplete
+          <el-input
           :disabled="disabled"
-          clearable
-          v-model="ruleForm.methodName"
-          :fetch-suggestions="querySearchAsync"
+          v-model="ruleForm.method_title"
           placeholder="请输入内容"
-          @select="handleSelect"
-          ></el-autocomplete>
+          ></el-input>
         </el-form-item>
 
         <!-- 单位 -->
         <el-form-item label="单位">
-          <el-select v-model="ruleForm.unit" multiple placeholder="请选择">
+          <el-select v-model="ruleForm.uom" multiple placeholder="请选择">
             <el-option
               v-for="item in options"
               :key="item.value"
@@ -176,17 +169,17 @@
 
         <!-- 有效位数 -->
         <el-form-item label="有效位数">
-          <el-input v-model="ruleForm.significant"></el-input>
+          <el-input v-model="ruleForm.significand"></el-input>
         </el-form-item>
 
         <!-- 精度 -->
         <el-form-item label="精度">
-          <el-input v-model="ruleForm.priority"></el-input>
+          <el-input v-model="ruleForm.decimals"></el-input>
         </el-form-item>
 
         <!-- 优先级 -->
         <el-form-item label="优先级">
-          <el-input v-model="ruleForm.priority"></el-input>
+          <el-input v-model="ruleForm.weight"></el-input>
         </el-form-item>
 
       </el-form>
@@ -194,6 +187,78 @@
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
         <el-button type="primary" @click="dialogVisible = false;addDataList()">确定</el-button>
+      </span>
+      </el-dialog>
+    </div>
+
+    <div class="analyseDialog">
+      <!-- edit -- analyse弹框 -->
+      <el-dialog
+      title="edit"
+      :visible.sync="editDialogVisible"
+      width="28%">
+
+      <!-- from -->
+      <el-form
+       :model="ruleForm"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm">
+
+        <!-- 因子 -->
+        <el-form-item label="因子">
+          <el-input
+           :disabled="!disabled"
+           v-model="ruleForm.factor_title">
+          </el-input>
+        </el-form-item>
+
+        <!-- 方法 -->
+        <el-form-item label="方法">
+          <el-input
+          :disabled="!disabled"
+          v-model="ruleForm.method_title"
+          ></el-input>
+        </el-form-item>
+
+        <!-- 单位 -->
+        <el-form-item label="单位">
+          <el-select v-model="ruleForm.uom" multiple placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+        <!-- 章节 -->
+        <el-form-item label="章节">
+          <el-input v-model="ruleForm.chapter"></el-input>
+        </el-form-item>
+
+        <!-- 有效位数 -->
+        <el-form-item label="有效位数">
+          <el-input v-model="ruleForm.significand"></el-input>
+        </el-form-item>
+
+        <!-- 精度 -->
+        <el-form-item label="精度">
+          <el-input v-model="ruleForm.decimals"></el-input>
+        </el-form-item>
+
+        <!-- 优先级 -->
+        <el-form-item label="优先级">
+          <el-input v-model="ruleForm.weight"></el-input>
+        </el-form-item>
+
+      </el-form>
+      <br>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false">取 消</el-button>
+        <el-button type="primary"
+         @click.native.prevent="editDialogVisible = false;handleEdit(ruleForm)">确 定</el-button>
       </span>
       </el-dialog>
     </div>
@@ -218,36 +283,42 @@ export default {
       timeout: null,
       layout: "total, prev, pager, next, jumper",
       dialogVisible: this.$store.state.dialogVisible, // 弹出显示隐藏状态 -- 默认为 -- 隐藏/false
+      editDialogVisible: this.$store.state.editDialogVisible,
       disabled: this.$store.state.disabled, // 禁用状态
       factorValue: '', // 因子查询
       methodValue: '', // 方法查询
       result: [], // 存放满足查询条件的数据
       options: [{
-        value: '选项1',
+        value: 'cm',
         label: 'cm',
       }, {
-        value: '选项2',
+        value: 'mol/L',
         label: 'mol/L',
       }, {
-        value: '选项3',
+        value: 'L',
         label: 'L',
       }, {
-        value: '选项4',
+        value: 'mm',
         label: 'mm',
       }, {
-        value: '选项5',
+        value: 'mol/KG',
         label: 'mol/KG',
       }],
       ruleForm: {
-        factorName: '', // 因子
-        methodName: '', // 方法
-        unit: '', // 单位
-        chapter: '', // 章节
-        significant: '', // 有效位数
-        priority: '0', // 优先级
+        id: '',
+        factor_id: '',
+        method_id: '',
+        factor_title: '',
+        method_title: '',
+        method_sn: '',
+        chapter: '',
+        uom: '',
+        significand: '',
+        decimals: '',
+        weight: '',
       },
       rules: {
-        factorName: [
+        factor_title: [
           { required: true, message: '请输入因子名称', trigger: 'blur' },
         ],
         methodName: [
@@ -261,9 +332,6 @@ export default {
   },
   created() {
     this.getData(); // 在页面开始时获取导数据
-  },
-  mounted() {
-    this.restaurants = this.loadAll();
   },
   methods: {
     reset() { // 重置
@@ -319,6 +387,9 @@ export default {
           id : this.currentData.id, // key
           factor_title : this.currentData.factor_title, // 因子
           method_title : this.currentData.method_title, // 方法
+          factor_id : this.currentData.factor_id, // 因子
+          method_id : this.currentData.method_id, // 方法
+          method_sn: this.currentData.method_sn,
           chapter : this.currentData.chapter, // 章节
           uom : this.currentData.uom, // 单位
           significand : this.currentData.significand, // 有效位数
@@ -344,15 +415,43 @@ export default {
         this.dialogVisible = true;
       }
     },
+    isEditPop(editDialogVisible) {
+      if(editDialogVisible == true) {
+        this.editDialogVisible = false;
+      }else {
+        this.editDialogVisible = true;
+      }
+    },
     addEmptyData() { // 每次更新清空列表
       this.ruleForm = {
+        id: '',
+        factor_id: '',
+        method_id: '',
         factor_title: '',
         method_title: '',
+        method_sn: '',
         chapter: '',
         uom: '',
         significand: '',
         decimals: '',
         weight: '',
+      };
+    },
+    editDialog(scope) { // 编辑框获取到数据
+      console.log(scope);
+      console.log(scope.row);
+      this.ruleForm = {
+        id: scope.row.id,
+        factor_id: scope.row.factor_id,
+        method_id: scope.row.method_id,
+        factor_title: scope.row.factor_title,
+        method_title: scope.row.method_title,
+        method_sn: scope.row.method_sn,
+        chapter: scope.row.chapter,
+        uom: scope.row.uom,
+        significand: scope.row.significand,
+        decimals: scope.row.decimals,
+        weight: scope.row.weight,
       };
     },
     addDataList() { // 新增
@@ -361,8 +460,12 @@ export default {
       console.log(this.$store.state.token);
       this.$http.post('/api/analyse',
       {
+        id: this.ruleForm.id,
+        factor_id: this.ruleForm.factor_id,
+        method_id: this.ruleForm.method_id,
         factor_title: this.ruleForm.factor_title,
         method_title: this.ruleForm.method_title,
+        method_sn: this.ruleForm.method_sn,
         chapter: this.ruleForm.chapter,
         uom: this.ruleForm.uom,
         significand: this.ruleForm.significand,
@@ -398,10 +501,14 @@ export default {
       console.log('编辑');
       console.log(scope.id);
       console.log(this.$store.state.token);
-      this.$http.put(`/api/analyse /${scope.id}`,
+      this.$http.put(`/api/analyse/${scope.id}`,
       {
+        id: this.ruleForm.id,
+        factor_id: this.ruleForm.factor_id,
+        method_id: this.ruleForm.method_id,
         factor_title: this.ruleForm.factor_title,
         method_title: this.ruleForm.method_title,
+        method_sn: this.ruleForm.method_sn,
         chapter: this.ruleForm.chapter,
         uom: this.ruleForm.uom,
         significand: this.ruleForm.significand,
@@ -417,7 +524,7 @@ export default {
         .then((response) => { // 请求成功
           if (this.$store.state.token) {
             console.log(response);
-            this.$message({ //message进入弹框 ：显示 '登录成功！'
+            this.$message({ //message进入弹框 ：显示 '成功！'
               message: '编辑成功！',
               type: 'success',
               duration: 1500,
@@ -590,73 +697,73 @@ export default {
       this.total = this.listData.length;
       console.log(this.total);
     },
-    loadAll() { // 弹窗搜索
-      return [
-        { value: '三全鲜食（北新泾店）', address: '长宁区新渔路144号' },
-        { value: 'Hot honey 首尔炸鸡（仙霞路）', address: '上海市长宁区淞虹路661号' },
-        { value: '新旺角茶餐厅', address: '上海市普陀区真北路988号创邑金沙谷6号楼113' },
-        { value: '泷千家(天山西路店)', address: '天山西路438号' },
-        { value: '胖仙女纸杯蛋糕（上海凌空店）', address: '上海市长宁区金钟路968号1幢18号楼一层商铺18-101' },
-        { value: '贡茶', address: '上海市长宁区金钟路633号' },
-        { value: '豪大大香鸡排超级奶爸', address: '上海市嘉定区曹安公路曹安路1685号' },
-        { value: '茶芝兰（奶茶，手抓饼）', address: '上海市普陀区同普路1435号' },
-        { value: '十二泷町', address: '上海市北翟路1444弄81号B幢-107' },
-        { value: '星移浓缩咖啡', address: '上海市嘉定区新郁路817号' },
-        { value: '阿姨奶茶/豪大大', address: '嘉定区曹安路1611号' },
-        { value: '新麦甜四季甜品炸鸡', address: '嘉定区曹安公路2383弄55号' },
-        { value: 'Monica摩托主题咖啡店', address: '嘉定区江桥镇曹安公路2409号1F，2383弄62号1F' },
-        { value: '浮生若茶（凌空soho店）', address: '上海长宁区金钟路968号9号楼地下一层' },
-        { value: 'NONO JUICE  鲜榨果汁', address: '上海市长宁区天山西路119号' },
-        { value: 'CoCo都可(北新泾店）', address: '上海市长宁区仙霞西路' },
-        { value: '快乐柠檬（神州智慧店）', address: '上海市长宁区天山西路567号1层R117号店铺' },
-        { value: 'Merci Paul cafe', address: '上海市普陀区光复西路丹巴路28弄6号楼819' },
-        { value: '猫山王（西郊百联店）', address: '上海市长宁区仙霞西路88号第一层G05-F01-1-306' },
-        { value: '枪会山', address: '上海市普陀区棕榈路' },
-        { value: '纵食', address: '元丰天山花园(东门) 双流路267号' },
-        { value: '钱记', address: '上海市长宁区天山西路' },
-        { value: '壹杯加', address: '上海市长宁区通协路' },
-        { value: '唦哇嘀咖', address: '上海市长宁区新泾镇金钟路999号2幢（B幢）第01层第1-02A单元' },
-        { value: '爱茜茜里(西郊百联)', address: '长宁区仙霞西路88号1305室' },
-        { value: '爱茜茜里(近铁广场)', address: '上海市普陀区真北路818号近铁城市广场北区地下二楼N-B2-O2-C商铺' },
-        { value: '鲜果榨汁（金沙江路和美广店）', address: '普陀区金沙江路2239号金沙和美广场B1-10-6' },
-        { value: '开心丽果（缤谷店）', address: '上海市长宁区威宁路天山路341号' },
-        { value: '超级鸡车（丰庄路店）', address: '上海市嘉定区丰庄路240号' },
-        { value: '妙生活果园（北新泾店）', address: '长宁区新渔路144号' },
-        { value: '香宜度麻辣香锅', address: '长宁区淞虹路148号' },
-        { value: '凡仔汉堡（老真北路店）', address: '上海市普陀区老真北路160号' },
-        { value: '港式小铺', address: '上海市长宁区金钟路968号15楼15-105室' },
-        { value: '蜀香源麻辣香锅（剑河路店）', address: '剑河路443-1' },
-        { value: '北京饺子馆', address: '长宁区北新泾街道天山西路490-1号' },
-        { value: '饭典*新简餐（凌空SOHO店）', address: '上海市长宁区金钟路968号9号楼地下一层9-83室' },
-        { value: '焦耳·川式快餐（金钟路店）', address: '上海市金钟路633号地下一层甲部' },
-        { value: '动力鸡车', address: '长宁区仙霞西路299弄3号101B' },
-        { value: '浏阳蒸菜', address: '天山西路430号' },
-        { value: '四海游龙（天山西路店）', address: '上海市长宁区天山西路' },
-        { value: '樱花食堂（凌空店）', address: '上海市长宁区金钟路968号15楼15-105室' },
-        { value: '壹分米客家传统调制米粉(天山店)', address: '天山西路428号' },
-        { value: '福荣祥烧腊（平溪路店）', address: '上海市长宁区协和路福泉路255弄57-73号' },
-        { value: '速记黄焖鸡米饭', address: '上海市长宁区北新泾街道金钟路180号1层01号摊位' },
-        { value: '红辣椒麻辣烫', address: '上海市长宁区天山西路492号' },
-        { value: '(小杨生煎)西郊百联餐厅', address: '长宁区仙霞西路88号百联2楼' },
-        { value: '阳阳麻辣烫', address: '天山西路389号' },
-        { value: '南拳妈妈龙虾盖浇饭', address: '普陀区金沙江路1699号鑫乐惠美食广场A13' },
-      ];
-    },
-    querySearchAsync(queryString, cb) {
-      const restaurants = this.restaurants;
-      const results = queryString ? restaurants.filter(this.createStateFilter(queryString)) : restaurants;
+    // loadAll() { // 弹窗搜索
+    //   return [
+    //     { value: '三全鲜食（北新泾店）', address: '长宁区新渔路144号' },
+    //     { value: 'Hot honey 首尔炸鸡（仙霞路）', address: '上海市长宁区淞虹路661号' },
+    //     { value: '新旺角茶餐厅', address: '上海市普陀区真北路988号创邑金沙谷6号楼113' },
+    //     { value: '泷千家(天山西路店)', address: '天山西路438号' },
+    //     { value: '胖仙女纸杯蛋糕（上海凌空店）', address: '上海市长宁区金钟路968号1幢18号楼一层商铺18-101' },
+    //     { value: '贡茶', address: '上海市长宁区金钟路633号' },
+    //     { value: '豪大大香鸡排超级奶爸', address: '上海市嘉定区曹安公路曹安路1685号' },
+    //     { value: '茶芝兰（奶茶，手抓饼）', address: '上海市普陀区同普路1435号' },
+    //     { value: '十二泷町', address: '上海市北翟路1444弄81号B幢-107' },
+    //     { value: '星移浓缩咖啡', address: '上海市嘉定区新郁路817号' },
+    //     { value: '阿姨奶茶/豪大大', address: '嘉定区曹安路1611号' },
+    //     { value: '新麦甜四季甜品炸鸡', address: '嘉定区曹安公路2383弄55号' },
+    //     { value: 'Monica摩托主题咖啡店', address: '嘉定区江桥镇曹安公路2409号1F，2383弄62号1F' },
+    //     { value: '浮生若茶（凌空soho店）', address: '上海长宁区金钟路968号9号楼地下一层' },
+    //     { value: 'NONO JUICE  鲜榨果汁', address: '上海市长宁区天山西路119号' },
+    //     { value: 'CoCo都可(北新泾店）', address: '上海市长宁区仙霞西路' },
+    //     { value: '快乐柠檬（神州智慧店）', address: '上海市长宁区天山西路567号1层R117号店铺' },
+    //     { value: 'Merci Paul cafe', address: '上海市普陀区光复西路丹巴路28弄6号楼819' },
+    //     { value: '猫山王（西郊百联店）', address: '上海市长宁区仙霞西路88号第一层G05-F01-1-306' },
+    //     { value: '枪会山', address: '上海市普陀区棕榈路' },
+    //     { value: '纵食', address: '元丰天山花园(东门) 双流路267号' },
+    //     { value: '钱记', address: '上海市长宁区天山西路' },
+    //     { value: '壹杯加', address: '上海市长宁区通协路' },
+    //     { value: '唦哇嘀咖', address: '上海市长宁区新泾镇金钟路999号2幢（B幢）第01层第1-02A单元' },
+    //     { value: '爱茜茜里(西郊百联)', address: '长宁区仙霞西路88号1305室' },
+    //     { value: '爱茜茜里(近铁广场)', address: '上海市普陀区真北路818号近铁城市广场北区地下二楼N-B2-O2-C商铺' },
+    //     { value: '鲜果榨汁（金沙江路和美广店）', address: '普陀区金沙江路2239号金沙和美广场B1-10-6' },
+    //     { value: '开心丽果（缤谷店）', address: '上海市长宁区威宁路天山路341号' },
+    //     { value: '超级鸡车（丰庄路店）', address: '上海市嘉定区丰庄路240号' },
+    //     { value: '妙生活果园（北新泾店）', address: '长宁区新渔路144号' },
+    //     { value: '香宜度麻辣香锅', address: '长宁区淞虹路148号' },
+    //     { value: '凡仔汉堡（老真北路店）', address: '上海市普陀区老真北路160号' },
+    //     { value: '港式小铺', address: '上海市长宁区金钟路968号15楼15-105室' },
+    //     { value: '蜀香源麻辣香锅（剑河路店）', address: '剑河路443-1' },
+    //     { value: '北京饺子馆', address: '长宁区北新泾街道天山西路490-1号' },
+    //     { value: '饭典*新简餐（凌空SOHO店）', address: '上海市长宁区金钟路968号9号楼地下一层9-83室' },
+    //     { value: '焦耳·川式快餐（金钟路店）', address: '上海市金钟路633号地下一层甲部' },
+    //     { value: '动力鸡车', address: '长宁区仙霞西路299弄3号101B' },
+    //     { value: '浏阳蒸菜', address: '天山西路430号' },
+    //     { value: '四海游龙（天山西路店）', address: '上海市长宁区天山西路' },
+    //     { value: '樱花食堂（凌空店）', address: '上海市长宁区金钟路968号15楼15-105室' },
+    //     { value: '壹分米客家传统调制米粉(天山店)', address: '天山西路428号' },
+    //     { value: '福荣祥烧腊（平溪路店）', address: '上海市长宁区协和路福泉路255弄57-73号' },
+    //     { value: '速记黄焖鸡米饭', address: '上海市长宁区北新泾街道金钟路180号1层01号摊位' },
+    //     { value: '红辣椒麻辣烫', address: '上海市长宁区天山西路492号' },
+    //     { value: '(小杨生煎)西郊百联餐厅', address: '长宁区仙霞西路88号百联2楼' },
+    //     { value: '阳阳麻辣烫', address: '天山西路389号' },
+    //     { value: '南拳妈妈龙虾盖浇饭', address: '普陀区金沙江路1699号鑫乐惠美食广场A13' },
+    //   ];
+    // },
+    // querySearchAsync(queryString, cb) {
+    //   const restaurants = this.restaurants;
+    //   const results = queryString ? restaurants.filter(this.createStateFilter(queryString)) : restaurants;
 
-      clearTimeout(this.timeout);
-      this.timeout = setTimeout(() => {
-        cb(results);
-      }, 3000 * Math.random());
-    },
-    createStateFilter(queryString) {
-      return state => (state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
-    },
-    handleSelect(item) {
-      console.log(item);
-    },
+    //   clearTimeout(this.timeout);
+    //   this.timeout = setTimeout(() => {
+    //     cb(results);
+    //   }, 3000 * Math.random());
+    // },
+    // createStateFilter(queryString) {
+    //   return state => (state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+    // },
+    // handleSelect(item) {
+    //   console.log(item);
+    // },
     ...mapMutations(['setToken']),
   },
 };
